209486305d60fa09d6358e7f69c377ddcc3513fa
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-29 09:51:46 +0200

    docs(deps): initial commit with dev dependencies and basic start

---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "refactor",
+        "test",
+        "style",
+        "perf",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+  },
+};
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
