9a816afd6cdc5e7ebccb507e932eb23666522d66
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-29 11:17:07 +0200

    feat(db): connect to database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 import express from "express";
+import { connectDB } from "./db/connectDB";
+import { getEnvVariable } from "./utils/env";
 
 const app = express();
 
@@ -7,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(getEnvVariable("MONGO_URL"));
+    console.log("CONNECTED TO DB");
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
