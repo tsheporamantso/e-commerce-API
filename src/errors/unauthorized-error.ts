e95db1425cd8ae406738b3f0a4911d4a1d05dffd
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-29 16:51:21 +0200

    feat(errors): add unauthorized custom error class

--- src/errors/unauthorized-error.ts
+++ src/errors/unauthorized-error.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { CustomErrorAPI } from "./custom-error";
 
 export class UnauthorizedError extends CustomErrorAPI {
-  constructor(message: string, statusCode: number = StatusCodes.UNAUTHORIZED) {
+  constructor(message: string, statusCode: number = StatusCodes.FORBIDDEN) {
     super(message, statusCode);
   }
 }
