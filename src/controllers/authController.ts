3c22e833c5bd86265687e01f8b732ed770951334
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-30 12:51:10 +0200

    feat(controller): register user with hashed password and jwt token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,20 @@
 import { asyncWrapper } from "../middleware/async";
 import { StatusCodes } from "http-status-codes";
+import User from "../models/userModel";
+import { attachCookiesToResponse } from "../utils/cookies";
 
 export const register = asyncWrapper(async (req, res) => {
-  res.status(StatusCodes.OK).json("user registered");
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ ...req.body, role });
+  const token = user.createJWT();
+
+  attachCookiesToResponse(res, token);
+
+  res.status(StatusCodes.CREATED).json({
+    user: { name: user.name, userId: user._id, role: user.role },
+  });
 });
 
 export const login = asyncWrapper(async (req, res) => {
