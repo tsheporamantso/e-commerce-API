3b31e0a8b9a4e1e1a6351b9e75e88f8831b347c4
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-30 12:10:33 +0200

    feat(middleware): add express-rate-limit middleware

---
+++ src/middleware/rate-limiter.ts
@@ -0,0 +1,10 @@
+import rateLimit from "express-rate-limit";
+
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    msg: "Too many requests sent. Try again later.",
+  },
+});
