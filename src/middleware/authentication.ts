5a9e21ba8a8aed5ecefcb5080c755d317cc2452d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-30 14:03:09 +0200

    feat(middleware): add authentication middleware

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import CustomError from "../errors";
+import { getEnvVariable } from "../utils/env";
+
+interface JWTPayloadType {
+  userId: string;
+  name: string;
+  role: string;
+}
+
+export const authentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.signedCookies.token || req.cookies.token;
+
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
+  }
+
+  try {
+    const payload = jwt.verify(
+      token,
+      getEnvVariable("JWT_SECRET"),
+    ) as JWTPayloadType;
+    req.user = {
+      userId: payload.userId,
+      name: payload.name,
+      role: payload.role,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
+  }
+};
